c65cd0ebce276b476bfde91068f2cdf60643dd87
omriweinmann <omri.weinmann@gmail.com>
2025-08-30 15:31:22 -0700

    Basic Procedural Generation
    Makes simplex noise, then for each pixel it changes a tile on a tilemaplayer to correspond to it.

--- level.gd
+++ level.gd
@@ -14,14 +14,28 @@ func _ready() -> void:
 		print(seed)
 	var noise = NoiseTexture2D.new()
 	var fast_noise = FastNoiseLite.new()
+	fast_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	fast_noise.frequency = 0.01
 	fast_noise.seed = seed
 	noise.width = width
 	noise.height = height
 	noise.noise = fast_noise
 	$Noise.texture = noise
+	var vp_size = get_viewport().get_visible_rect().size
+	$Noise.position =  Vector2(vp_size[0]*0.5,vp_size[1]*0.5)
 	
+	await noise.changed
 
-
+	for x in width:
+		for y in height:
+			var noise_value = fast_noise.get_noise_2d(float(x),float(y))
+			#print(noise_value)
+			var noise_value_processed = floor(((noise_value+1)/2)*5)
+			#print(noise_value_processed)
+			$TileMapLayer.set_cell(Vector2i(x,y),0,Vector2i(0,noise_value_processed))
+			
+			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Restart"):
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
